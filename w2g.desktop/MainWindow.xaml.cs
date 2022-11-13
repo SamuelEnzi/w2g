342d79e66e5e317ddb3bfc5d93a1a3335e95cd8e
Schmuurf <i.sebastiani44@gmail.com>
2022-11-13 16:05:58 +0100

    created basic host window

--- w2g.desktop/MainWindow.xaml.cs
+++ w2g.desktop/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,19 +12,78 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using YoutubeExplode;
+using w2g.core;
 
 namespace w2g.desktop
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private double? postition;
+
+        public double? PostitionTime { get => postition; set
+            { 
+                postition = value; 
+                NotifyPropertyChanged();
+                UI_Video.Position = TimeSpan.FromSeconds(UI_Position.Value);
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            DispatcherTimer Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromSeconds(1);
+            Timer.Tick += Tick;
+            Timer.Start();
+
+            Action load = async () =>
+            {
+                var url = await Helper.ConvertToStream("https://www.youtube.com/watch?v=R_FQU4KzN7A");
+                Dispatcher.Invoke(() =>
+                {
+                    UI_Video.Source = new Uri(url.Item1);
+                    UI_Position.Minimum = 0;
+                    UI_Position.Maximum = url.Item2.Value.TotalSeconds;
+                });
+            };
+            load.Invoke();
+        }
+
+        private void Tick(object? sender, EventArgs e)
+        {
+            if (UI_Position.IsMouseOver)
+                return;
+            postition = UI_Video.Position.TotalSeconds;
+            UI_Position.Value = (double)postition;
+        }
+
+        private void UI_Play_Click(object sender, RoutedEventArgs e)
+        {
+            UI_Video.Play();
+        }
+
+        private void UI_Pause_Click(object sender, RoutedEventArgs e)
+        {
+            UI_Video.Pause();
+        }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UI_Position_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            this.PostitionTime = UI_Position.Value;
+
         }
     }
 }
