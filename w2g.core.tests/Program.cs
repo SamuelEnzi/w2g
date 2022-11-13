f2b24ed39211457b2b68d6a5b9f5c24d7224787c
Samuel Enzi <enzi.samuel1@gmail.com>
2022-11-13 16:09:22 +0100

    update program.cs

--- w2g.core.tests/Program.cs
+++ w2g.core.tests/Program.cs
@@ -12,17 +12,13 @@ else
 while(true)
     Console.ReadLine();
 
-
 void StartClient()
 {
     var client = new Client("127.0.0.1", 6061);
 
     client.SetCurrent += (s, i) => Display(i);
     client.SetTime += (s, i) => Display(i);
-    client.VideoSet += (s, i) =>
-    {
-        Display(i);
-    };
+    client.VideoSet += (s, i) => Display(i);
     client.VideoPlay += (s, i) => Display(i);
     client.VideoStop += (s, i) => Display(i);
 
@@ -31,11 +27,6 @@ void StartClient()
 
     Console.WriteLine("client connected to '127.0.0.1:6061'");
     client.RequestCurrent(new CurrentModel());
-}
-
-void Display(object i)
-{
-    Console.WriteLine(i.ToString());
 }
 
 void StartServer()
@@ -49,12 +40,12 @@ void StartServer()
     Console.WriteLine("press a key to send requests");
     Console.ReadLine();
 
-
     server.Play(new PlayModel { Seconds = 12 });
     server.Stop(new StopModel { Seconds = 123 });
     server.SendVideoUrl(new UrlModel { Url = "diocae" });
     server.Current(new CurrentModel { Url = "diocane2", Seconds = 124 });
     server.SetTime(new TimeModel { Seconds = 12345 });
-
-    
 }
+
+void Display(object i) =>
+     Console.WriteLine(i.ToString());
