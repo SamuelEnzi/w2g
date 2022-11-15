ebebdc814f58ba328a6ba8650972384ea7e749c5
Samuel Enzi <enzi.samuel1@gmail.com>
2022-11-15 20:36:53 +0100

    new ui

--- w2g.desktop/App.xaml.cs
+++ w2g.desktop/App.xaml.cs
@@ -13,5 +13,27 @@ namespace w2g.desktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var setup = new View.Setup();
+
+            View.UseMode useMode = setup.Display();
+
+            switch (useMode)
+            {
+                case View.UseMode.Connect:
+                    new View.Mode.Connect().ShowDialog();
+                    break;
+                case View.UseMode.Host:
+                    new View.Mode.Host().ShowDialog();
+                    break;
+                default:
+                    break;
+            }
+
+            Environment.Exit(0);
+        }
     }
 }
---
+++ w2g.desktop/View/Setup.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace w2g.desktop.View
+{
+    /// <summary>
+    /// Interaktionslogik für Setup.xaml
+    /// </summary>
+    public partial class Setup : Window
+    {
+        public UseMode UseMode { get; set; } = UseMode.Connect;
+
+        public Setup()
+        {
+            InitializeComponent();
+            UseMode = UseMode.Connect;
+            UI_Content.Content = new Pages.Connect();
+        }
+
+        private void OnApplicationExit(object sender, MouseButtonEventArgs e) =>
+            Environment.Exit(0);
+
+        private void OnWindowDragged(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+
+        public UseMode Display()
+        {
+            this.ShowDialog();
+            return UseMode;
+        }
+
+        private void OnConnectClicked(object sender, MouseButtonEventArgs e)
+        {
+            UseMode = UseMode.Connect;
+            UI_Content.Content = new Pages.Connect();
+        }
+
+        private void OnHostClicked(object sender, MouseButtonEventArgs e)
+        {
+            UseMode = UseMode.Host;
+            UI_Content.Content = new Pages.Host();
+        }
+
+        private void OnNextClicked(object sender, MouseButtonEventArgs e) =>
+            this.Close();
+    }
+
+    public enum UseMode
+    {
+        Connect,
+        Host
+    }
+}
