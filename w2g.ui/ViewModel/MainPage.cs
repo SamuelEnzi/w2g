434e904a2cb65ec5c095275c0609861f66189bc0
Samuel Enzi <enzi.samuel1@gmail.com>
2023-01-07 00:16:36 +0100

    restructured events

--- w2g.ui/ViewModel/MainPage.cs
+++ w2g.ui/ViewModel/MainPage.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SimpleTCP;
+using System;
+using System.Net.Sockets;
+using w2g.core.standart.Models;
 using w2g.ui.Types;
 using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
@@ -27,6 +30,7 @@ namespace w2g.ui.ViewModel
         public MainPage()
         {
             LoadVideo("https://music.youtube.com/watch?v=TQTISvU6o2Y");
+            SetupListeners();
         }
 
         public async void LoadVideo(string videoUrl)
@@ -45,5 +49,87 @@ namespace w2g.ui.ViewModel
             }
             VideoSource = video;
         }
+
+
+        private void SetupListeners()
+        {
+            switch (Helpers.Service.Instance.State)
+            {
+                case Helpers.ClientState.Host:
+                    SetupHostListeners();
+                    break;
+                case Helpers.ClientState.Client:
+                    SetupClientListeners();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// setup all host listeners
+        /// </summary>
+        private void SetupHostListeners()
+        {
+            Helpers.Service.Instance.server.ClientConnected += OnClientConnected;
+            Helpers.Service.Instance.server.ClientDisconnected += OnClientDisconnected;
+            Helpers.Service.Instance.server.OnCurrentRequest += OnCurrentRequested;
+        }
+
+        /// <summary>
+        /// setup all client listeners
+        /// </summary>
+        private void SetupClientListeners()
+        {
+            Helpers.Service.Instance.client.SetCurrent += OnCurrentSet;
+            Helpers.Service.Instance.client.VideoPlay += OnVideoPlay;
+            Helpers.Service.Instance.client.SetTime += OnSetTime;
+            Helpers.Service.Instance.client.VideoSet += OnVideoSet;
+            Helpers.Service.Instance.client.VideoStop += OnVideoStop;
+        }
+
+        #region host listeners
+        private void OnCurrentRequested(object sender, Message e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnClientDisconnected(object sender, TcpClient e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnClientConnected(object sender, TcpClient e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region client listeners
+        private void OnVideoStop(object sender, StopModel e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnVideoSet(object sender, UrlModel e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnSetTime(object sender, TimeModel e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnVideoPlay(object sender, PlayModel e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnCurrentSet(object sender, CurrentModel e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
